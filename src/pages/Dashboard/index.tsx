9adbbb477be7fa44fe7aa1c576f90f6f1d7c8e96
gabriel g <gabrielmgarcia96@gmail.com>
2021-11-10 17:07:18 -0300

    :sparkles: animaçoes css

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,7 +2,9 @@ import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi'
 import api from '../../services/api';
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
+
+import logoImg from '../../assets/GithubLogo.svg';
 
 interface Repository {
   full_name: string,
@@ -17,23 +19,35 @@ interface Repository {
 const Dashboard: React.FC = () => { 
   const [newRepo, setNewRepo] = useState('');
   const [repos, setRepos] = useState<Repository[]>([]);
+  const [inputError, setInputError] = useState('');
   
   async function handleAddRepo(event: FormEvent) {
     event.preventDefault();
+    
+    if(!newRepo) {
+      setInputError('Digite nome/autor do repositório.');
+      return;
+    }
+    
+    try {
+      const response = await api.get(`repos/${newRepo}`);
 
-    const response = await api.get(`repos/${newRepo}`);
+      const repository = response.data;
 
-    const repository = response.data;
+      setRepos([...repos, repository]);
 
-    setRepos([...repos, repository]);
-
-    setNewRepo('');
+      setNewRepo('');
+      setInputError('');
+    } catch (Err) {
+      setInputError('Repositório não encontrado.');
+    }
   }
 
   return (
     <>
+      <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
-      <Form onSubmit={handleAddRepo}>
+      <Form hasError={!!inputError} onSubmit={handleAddRepo}>
         <input 
           value={newRepo}
           onChange={e => setNewRepo(e.target.value)}
@@ -41,6 +55,9 @@ const Dashboard: React.FC = () => {
         />
         <button>Pesquisar</button>
       </Form>
+
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
         {repos.map(repo => (
           // eslint-disable-next-line react/jsx-no-target-blank
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,8 +1,11 @@
-import styled from 'styled-components';
-
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
 
-export const Title = styled.h1 `
+interface FormProps {
+  hasError: boolean;
+}
+
+export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
   max-width: 450px;
@@ -10,7 +13,7 @@ export const Title = styled.h1 `
   margin-top: 80px;
 `
 
-export const Form = styled.form `
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
 
@@ -23,9 +26,39 @@ export const Form = styled.form `
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
-    border-color: #c53030;
+    box-shadow: 0 2px 4px rgba(0,0,0,0.2);
     border: 1px solid #fff;
-    border-right: 0;
+
+    &:focus, &:hover {
+      box-shadow: 0 4px 8px rgba(0,0,0,0.2);
+      transition: box-shadow 0.1s;
+    }
+
+    ${props => props.hasError && css`
+      border: 1px solid #ff0000;
+      border-right: 0;
+      animation: shake 0.50s cubic-bezier(.36,.07,.19,.97) both ;
+      transform: translate3d(0, 0, 0);
+      backface-visibility: hidden;
+
+      @keyframes shake {
+        10%, 90% {
+          transform: translate3d(-1px, 0, 0);
+        }
+        
+        20%, 80% {
+          transform: translate3d(1px, 0, 0);
+        }
+
+        30%, 50%, 70% {
+          transform: translate3d(-2px, 0, 0);
+        }
+
+        40%, 60% {
+          transform: translate3d(2px, 0, 0);
+        }
+      }
+    `}
   }
 
   &::placeholder {
@@ -35,19 +68,31 @@ export const Form = styled.form `
   button {
     width: 210px;
     height: 70px;
-    background: #728bd4;
+    background: #4169e1;
     border-radius: 0 5px 5px 0;
     border: 0;
     color: #fff;
+    font-weight: bold;
     transition: 0.1s;
 
     &:hover {
-      background: ${shade(0.1, '#728bd4')};
+      background: ${shade(0.1, '#4169e1')};
+    }
+
+    &:active {
+      transform: scale(0.96);
     }
   }
 `
 
-export const Repositories = styled.div `
+export const Error = styled.span `
+  display: block;
+  color: #ff0000;
+  margin-top: 12px;
+  font-size: 14px;
+`
+
+export const Repositories = styled.div`
   margin-top: 80px;
   max-width: 700px;
 
